7f365fe8d704cadd925e926d41fafa73d5c8d243
Arijana Dekanovic <arijana.dekanovic18@gmail.com>
2024-08-11 00:15:27 +0200

    Docker setup fix

--- RRMSAPI/src/RRMS.SharedKernel/Options/AccessTokenOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WorkFlow.SharedKernel.Settings;
-
-public class AccessTokenOptions
-{
-    public string SecurityKey { get; init; }
-    public long DurationInSeconds { get; init; }
-    public string Issuer { get; init; }
-    public string Audience { get; init; }
-}
