e73822391813137df845158234dad661fc62b834
Arijana Dekanovic <arijana.dekanovic18@gmail.com>
2024-08-14 23:06:20 +0200

    Docker compose updated to persist containers across PC restarts

--- rrms/lib/config/app_settings/development_app_settings.dart
+++ rrms/lib/config/app_settings/development_app_settings.dart
@@ -1,7 +1,7 @@
 import 'package:rrms/_all.dart';
 
 AppSettings developmentAppSettings = AppSettings(
-  mainApiUrl: 'https://55ec-2a02-27b0-5500-2020-787e-adbf-554d-484d.ngrok-free.app',
+  mainApiUrl: 'https://f023-2a02-27b0-5500-76f0-30a3-bc0e-7c23-9d8f.ngrok-free.app',
   storageApiUrl: '',
   // resetStorage: true,
 );
