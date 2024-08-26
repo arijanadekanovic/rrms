613e8ae3810f033ba6bc0e6ae78b223e30e77b5b
Arijana Dekanovic <arijana.dekanovic18@gmail.com>
2024-08-26 22:44:33 +0200

    dev config

--- rrms/lib/config/app_settings/development_app_settings.dart
+++ rrms/lib/config/app_settings/development_app_settings.dart
@@ -1,7 +1,7 @@
 import 'package:rrms/_all.dart';
 
 AppSettings developmentAppSettings = AppSettings(
-  mainApiUrl: 'https://cca0-2a02-27b0-5506-15d0-c000-a723-415-110c.ngrok-free.app',
+  mainApiUrl: 'https://f2b9-2a02-27b0-5500-eeb0-bd37-9eef-7bd5-209c.ngrok-free.app',
   storageApiUrl: '',
   // resetStorage: true,
 );
