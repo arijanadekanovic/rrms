22e6c64920d880e213556830fc4c34ac6de2acd4
sefika96 <sg@ksquadrat.de>
2024-08-27 18:25:06 +0200

    PAYMENTS - Add Payment POST method commands

---
+++ RRMSAPI/src/RRMS.Application/Features/Payment/Commands/PaymentsAddCommand/PaymentsAddCommandResult.cs
@@ -0,0 +1,9 @@
+﻿namespace RRMS.Application.Features.Payment.Commands.PaymentsAddCommand
+{
+    internal class PaymentsAddCommandResult
+    {
+            public double Amount { get; set; }
+            public int ResidentId { get; set; }
+
+    }
+}
