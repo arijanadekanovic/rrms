3e98372f579e4b4d968cd3b7b135ce2b8b839a11
Arijana Dekanovic <arijana.dekanovic18@gmail.com>
2024-09-01 22:07:17 +0200

    Refresh message groups when message added

--- rrms/lib/config/app_settings/development_app_settings.dart
+++ rrms/lib/config/app_settings/development_app_settings.dart
@@ -1,7 +1,7 @@
 import 'package:rrms/_all.dart';
 
 AppSettings developmentAppSettings = AppSettings(
-  mainApiUrl: 'https://6bad-109-163-174-220.ngrok-free.app',
-  storageApiUrl: 'https://8a75-109-163-174-220.ngrok-free.app',
+  mainApiUrl: 'https://b102-109-163-168-40.ngrok-free.app',
+  storageApiUrl: 'https://a7b5-109-163-168-40.ngrok-free.app',
   // resetStorage: true,
 );
