4aa4dd72717b359784430d01760082971235f6ad
sefika96 <sg@ksquadrat.de>
2024-09-03 16:27:57 +0200

    add residences history query

---
+++ RRMSAPI/src/RRMS.Application/Features/Residence/Queries/ResidencesHistoryQuery/ResidencesHistoryQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RRMS.Application.Abstractions.Persistance;
+using RRMS.Microservices.Application.Abstractions.Services.Identity;
+using RRMS.Microservices.SharedKernel.Messaging;
+using RRMS.Microservices.SharedKernel.Primitives;
+
+namespace RRMS.Application.Features;
+
+public sealed class ResidencesHistoryQueryHandler : IQueryHandler<ResidencesHistoryQuery, List<ResidencesHistoryQueryResult>>
+{
+    private readonly IDatabaseContext _databaseContext;
+    private readonly ICurrentUser _currentUser;
+
+    public ResidencesHistoryQueryHandler
+    (
+        IDatabaseContext databaseContext,
+        ICurrentUser currentUser
+    )
+    {
+        ArgumentNullException.ThrowIfNull(databaseContext);
+        ArgumentNullException.ThrowIfNull(currentUser);
+
+        _databaseContext = databaseContext;
+        _currentUser = currentUser;
+    }
+
+    public async Task<Result<List<ResidencesHistoryQueryResult>>> Handle(ResidencesHistoryQuery request, CancellationToken cancellationToken)
+    {
+        var residences = await _databaseContext.Residences
+                .Where(x => !x.IsDeleted && x.Residents.Any(r => r.UserId == _currentUser.Id))
+                .Include(x => x.City)
+                .ToListAsync(cancellationToken);
+
+        return residences.Select(x => new ResidencesHistoryQueryResult
+        {
+            Name = x.Name,
+            Address = x.Address,
+            Rooms = x.Rooms,
+            Size = x.Size,
+            RentPrice = x.RentPrice,
+            ThumbnailUrl = x.ThumbnailUrl,
+            CityId = x.CityId,
+            City = x.City.Name
+        }).ToList();
+    }
+}
---
+++ RRMSAPI/src/RRMS.Application/Features/Residence/Queries/ResidencesHistoryQuery/ResidencesHistoryQueryResult.cs
@@ -0,0 +1,13 @@
+﻿namespace RRMS.Application.Features;
+
+public sealed record ResidencesHistoryQueryResult
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public int Rooms { get; set; }
+    public double Size { get; set; }
+    public double RentPrice { get; set; }
+    public string ThumbnailUrl { get; set; }
+    public int CityId { get; set; }
+    public string City { get; set; }
+}
