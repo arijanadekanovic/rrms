dbef90d76da718bfb66547d7c0ba33765519efeb
arijanadekanovic <arijana.dekanovic18@gmail.com>
2024-09-26 23:48:06 +0200

    minor fixes

--- rrms/lib/config/app_settings/development_app_settings.dart
+++ rrms/lib/config/app_settings/development_app_settings.dart
@@ -1,6 +1,3 @@
 import 'package:rrms/_all.dart';
 
-AppSettings developmentAppSettings = AppSettings(
-  mainApiUrl: 'https://10.0.2.2:5001',
-  storageApiUrl: 'https://10.0.2.2:5003',
-);
+AppSettings developmentAppSettings = AppSettings();
