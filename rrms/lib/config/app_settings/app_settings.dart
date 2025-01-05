46af507f9f282a3bcb8684ddc9fcc27ec2b1caa1
arijanadekanovic <arijana.dekanovic18@gmail.com>
2025-01-05 22:54:41 +0100

    updates & fixes

--- rrms/lib/config/app_settings/app_settings.dart
+++ rrms/lib/config/app_settings/app_settings.dart
@@ -1,54 +1,23 @@
 import 'package:rrms/_all.dart';
 
 class AppSettings {
-  final AppSettingsApiUrl apiUrlMobile;
-  final AppSettingsApiUrl apiUrlDesktop;
+  final String mainApiUrl;
+  final String storageApiUrl;
   final bool resetStorage;
   final String appName;
   final PayPalAppSettings payPalAppSettings;
   final LoggingOptions loggingOptions;
 
   AppSettings({
-    this.apiUrlMobile = const AppSettingsApiUrl(
-      mainApiUrl: 'https://10.0.2.2:5001',
-      storageApiUrl: 'https://10.0.2.2:5003',
-    ),
-    this.apiUrlDesktop = const AppSettingsApiUrl(
-      mainApiUrl: 'https://localhost:5001',
-      storageApiUrl: 'https://localhost:5003',
-    ),
+    this.mainApiUrl = const String.fromEnvironment('mainApiUrl'),
+    this.storageApiUrl = const String.fromEnvironment('storageApiUrl'),
     this.resetStorage = false,
     this.appName = 'RRMS',
     this.payPalAppSettings = const PayPalAppSettings(
-      clientId: 'AbHQr8FP8YjRNclQTc7-nfHsyFLP0l0DFp-UCsJhfWtI0E-DYDaukjciqqFbJhefRLoMDTTVSrO3fAqH',
-      secretKey: 'EASMvGL_wHPZGj9KVn3pDt5gIsdYRuvXgP_bZdKlC6u4PmV1WKGbWwcA1o3zQZxaMZj9oPdfHzPD9R14',
+      clientId: const String.fromEnvironment('paypalClientId'),
+      secretKey: const String.fromEnvironment('paypalSecretKey'),
     ),
     this.loggingOptions = const LoggingOptions(),
-  });
-
-  String get mainApiUrl => _isMobile
-      ? apiUrlMobile.mainApiUrl
-      : _isDesktop
-          ? apiUrlDesktop.mainApiUrl
-          : '<UNSUPPORTED PLATFORM>';
-
-  String get storageApiUrl => _isMobile
-      ? apiUrlMobile.storageApiUrl
-      : _isDesktop
-          ? apiUrlDesktop.storageApiUrl
-          : '<UNSUPPORTED PLATFORM>';
-
-  bool get _isDesktop => Platform.isWindows || Platform.isLinux || Platform.isMacOS;
-  bool get _isMobile => Platform.isAndroid || Platform.isIOS;
-}
-
-class AppSettingsApiUrl {
-  final String mainApiUrl;
-  final String storageApiUrl;
-
-  const AppSettingsApiUrl({
-    required this.mainApiUrl,
-    required this.storageApiUrl,
   });
 }
 
